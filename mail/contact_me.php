3e105b539d0862b485258e531c8fd60d90cd5934
Marcel Engelmann <engelmann.marcel@yahoo.de>
2015-03-10 00:51:18 +0100

    Make contact form work

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,18 +13,17 @@ if(empty($_POST['name'])  		||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
-$message = wordwrap($$_POST['message'], 70);
+$message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'roadgenius@googlegroups.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to1 = 'engelmann.marcel@yahoo.de'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Roadgenius - Nachricht von $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\n Message: $message\n\n";
 $headers = "From: noreply@roadgenius.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 
-$headers =  'From:' . $email_address . ' . "\r\n" .
-            'Reply-To:' . $email_address . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
 mail($to,$email_subject,$email_body,$headers);
+mail($to1,$email_subject,$email_body,$headers);
 return true;
 ?>
